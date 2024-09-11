a131626112d128d58120a26a26bbb773a0f34d48
Tristan Vegas <vegastristan1@gmail.com>
2024-09-11 19:05:57 +0800

    updated Login Function

--- Lending Project Application/Login.vb
+++ Lending Project Application/Login.vb
@@ -1,7 +1,27 @@
-﻿Public Class Login
+﻿Imports System.Data.SqlClient
+
+Public Class Login
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles login_btn_login.Click
-        Me.Hide()
-        Dashboard.Show()
+
+        Dim con As New SqlConnection("Data Source=LENOVO-GEN7\SQLEXPRESS;Initial Catalog=dbLending;Integrated Security=True;TrustServerCertificate=True")
+        con.Open()
+        Dim query As String = "SELECT COUNT(*) FROM Users WHERE Username=@username AND PasswordHash=@passwordHash"
+        Dim cmd As New SqlCommand(query, con)
+        cmd.Parameters.AddWithValue("@username", login_txt_username.Text)
+        cmd.Parameters.AddWithValue("@passwordHash", login_txt_password.Text)
+
+        Dim count As Integer = Convert.ToInt64(cmd.ExecuteScalar)
+        con.Close()
+
+        If count > 0 Then
+            'MessageBox.Show("Login Successfull!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Me.Hide()
+            Dashboard.Show()
+        Else
+            MessageBox.Show("Login Error")
+        End If
+
+
     End Sub
 
     Private Sub btnCloseLogin_Click(sender As Object, e As EventArgs) Handles btnCloseLogin.Click
